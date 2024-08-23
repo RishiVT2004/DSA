a0fc05c92fd5de6a4843f7def48e761a3a009468
Rishivatsal Mishra <121601369+RishiVT2004@users.noreply.github.com>
2024-08-23 22:41:36 +0530

    Create  Minimum Window Substring .java

---
+++ SlidingWindow/hard/Minimum Window Substring .java
@@ -0,0 +1,19 @@
+class Solution {
+    public String minWindow(String a, String b) {
+       
+        /*
+            a = "abcd" ; b = "bd"
+            min size of windoe => 2
+            windows(len=2) => [[a,b],c,d],[a,[b,c],d],[a,b,[c,d]]
+            windows(len=3) => [[a,b,c],d] , [a,[b,c,d]] -> this is the correct answer
+            ans -: bcd
+
+            I was able to understand the logic and how the pointers would work , when the 
+            windows would slide and when windows will increase in size , 
+            but i was unable to implement this in code as i didn't understand how to 
+            properly search and return the strings 
+        */
+
+        // to be attempted after stacks 
+    }
+}
