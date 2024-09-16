d4d0f7fb7dca5ba4828df92a219412f634467b2b
Rishivatsal Mishra <121601369+RishiVT2004@users.noreply.github.com>
2024-09-16 12:34:27 +0530

    Create CreateDoubleLL.java

---
+++ DoubleLinkedList/Easy/CreateDoubleLL.java
@@ -0,0 +1,57 @@
+You are given an array ‘Arr’ of size ‘N’ consisting of positive integers.
+Your task is to make a doubly linked list from the array and return the head of the linked list.
+Here, the head of the doubly linked list is the first element of the array, and the tail of the doubly linked list is the last element.
+Note:
+A doubly linked list is one in which it is possible to access the next and the previous nodes from a node in the linked list (if they exist).
+
+  /****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+     public Node prev;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data, Node next, Node prev)
+     {
+         this.data = data;
+         this.next = next;
+         this.prev = prev;
+     }
+ };
+
+ *****************************************************************/
+
+public class Solution
+{
+    public static Node constructDLL(int []arr) {
+        // Write your code here
+        if(arr.length == 0) return null;
+
+        Node head = new Node(arr[0]);
+        Node curr = head;
+
+        for(int i = 1;i<arr.length ; i++){
+            Node nextNode = new Node(arr[i]);
+            curr.next = nextNode;
+            nextNode.prev = curr;
+            curr = nextNode;
+        }
+        return head;
+    }
+}
