af9857c9db7bba589463e2151cd0913b3c5029e7
Rishivatsal Mishra <121601369+RishiVT2004@users.noreply.github.com>
2024-10-31 17:57:31 +0530

    Update validPalindrome.java

--- 2pointers/easy/validPalindrome.java
+++ 2pointers/easy/validPalindrome.java
@@ -3,24 +3,28 @@ class Solution {
 
 // 1. initial draft , solved completly by mylself , and checkef chatgpt for some java methods to convert to lower case and replace the non aplha num character by ""
 
+        class Solution {
+    public boolean isPalindrome(String s) {
         s = s.toLowerCase().replaceAll("[^A-Za-z0-9]" , ""); // takes O(N) space 
-
-        int i = 0;
+        
         int n = s.length();
-        int j = s.length()-1;
+        int left = 0;
+        int right = s.length()-1;
 
         if(n == 0 || n == 1) return true; 
 
-        while(i <= n/2){           
-            if((s.charAt(i) == s.charAt(j))){
-                j--;
-                i++;
+        while(left <= n/2){           
+            if((s.charAt(left) == s.charAt(right))){
+                left++;
+                right--;
                 continue;
             }else{
                 return false;
             }
         }
         return true;
+    }
+}
 
       //space -> O(N)
       //Time -> O(N)
